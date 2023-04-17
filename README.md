9aea9a3212f3103071b2f1522242e874c7cf1640
Mohammedswisse <108531359+Mohammedswisse@users.noreply.github.com>
2023-04-17 23:51:10 +0200

    I finished the project

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # js-countdown
 this app for count down time
 add some text 
+
+## git add .
+## git commit -m 'what you did '
+## git push 
--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-const days = document.querySelectorAll('days');
-const hours = document.querySelectorAll('hours');
-const minutes = document.querySelectorAll('minutes');
-const seconds = document.querySelectorAll('seconds');
+<!-- Display the countdown timer in an element -->
+<
+p id = "demo" > < /p>
 
-const currentYear = new Date().getFullYear();
+<
+script >
+    //Set the date we're counting down to
+    var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
-const newYearTime = new Date(`january 1 $
- {currentYear + 1} 00:00:00`);
-//function
-function updateCountdowntime() {
-    const currentTime = new Date();
-    const diff = newYearTime - currentTime;
+//Update the count down every 1 second
+var x = setInterval(function() {
 
-    const d = Math.floor(diff / 1000 / 68 / 68 / 24);
+    // Get today's date and time
+    var now = new Date().getTime();
 
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-    console.log(d);
-}
-updateCountdowntime();
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000); <
+/script>
