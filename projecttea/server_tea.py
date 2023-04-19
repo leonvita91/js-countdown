dcbc03b05a156d0e0ca2fe384037ad2888ebe541
leonvita91 <leonvita91@gmail.com>
2023-04-19 20:46:35 +0100

    add tea-project

---
+++ projecttea/server_tea.py
@@ -0,0 +1,12 @@
+#bin/venv python
+
+
+from flask import Flask , render_template
+
+app = Flask(__name__)
+@app.route("/")
+def camel():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost' ,port=5000 , debug=True)
