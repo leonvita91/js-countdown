1d6b7d41957096f02944b076fd9a60455f937f83
leonvita91 <leonvita91@gmail.com>
2023-04-19 15:34:36 +0100

    fix mobile issue

--- projectcamel/server.py
+++ projectcamel/server.py
@@ -9,4 +9,4 @@ def camel():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0,0,0,0' ,port=5000 , debug=True)
+    app.run(host='localhost' ,port=5000 , debug=True)
